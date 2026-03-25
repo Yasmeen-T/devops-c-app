898c212cbed6106f45ed058c47f17c999f5fcfb5
Yasmeen-T <yasmeentaj0114@gmail.com>
2026-03-25 04:25:40 +0000

    feat: add simple menu text

--- app.c
+++ app.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-
 int main() {
     char name[50];
     printf("Welcome to DevOps C App\n");
     printf("Enter your name: ");
     scanf("%s", name);
     printf("Hello, %s!\n", name);
+    
+    // PART 5 CONFLICT AREA
+    printf("\n--- Main Menu ---\n");
+    printf("1. Use Calculator\n");
+    printf("2. Exit\n");
+
     return 0;
 }

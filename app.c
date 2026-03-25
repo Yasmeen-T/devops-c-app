4cb215a38abe3e9da6b0a8e8b78d637339e11e29
Yasmeen-T <yasmeentaj0114@gmail.com>
2026-03-25 04:17:32 +0000

    feat: add user greeting functionality

--- app.c
+++ app.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
+    char name[50];
     printf("Welcome to DevOps C App\n");
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
     return 0;
 }
